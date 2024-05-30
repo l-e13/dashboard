1ff318abd961d37ea30685aa45636d066c01e80d
l-e13 <156118405+l-e13@users.noreply.github.com>
2024-05-29 20:05:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.title("ACL Dashboard")  # title
 st.write("Apply filters to see non-blank record counts for variables.")
 
 # upload dataset in pandas
-data = pd.read_excel("Dashboard Headers.xlsx")
+data = pd.read_excel("Dashboard Headers Test Data.xlsx")
 
 # defining variables to count non blank records
 variables = [
